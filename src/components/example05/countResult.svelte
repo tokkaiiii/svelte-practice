5f50b9fda32108188b8f4c27f3dd3d1426ad2b09
tokkaiiii <luv_3_u@naver.com>
2024-11-19 15:02:54 +0900

    :sparkles: feat(day1-5): store 예제
    - store 예제

---
+++ src/components/example05/countResult.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { count } from "../../stores.js";
+</script>
+
+<h1>현재 count는 {$count} 입니다.</h1>
---
+++ src/routes/example05/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Increment from "../../components/example05/increment.svelte";
+    import Decrement from "../../components/example05/decrement.svelte";
+    import CountResult from "../../components/example05/countResult.svelte";
+</script>
+<CountResult/>
+<Increment/>
+<Decrement/>
