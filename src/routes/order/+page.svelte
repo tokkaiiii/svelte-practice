cd5ddda99f139265717067b8be8f4f0e679aa167
tokkaiiii <luv_3_u@naver.com>
2024-11-19 09:36:12 +0900

    :sparkles: feat(day1-1): 연습
    - 클릭시 카운트 올라감

---
+++ src/routes/order/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {goto} from "$app/navigation";
+
+    function handleClick() {
+        console.log('주문을 처리중입니다')
+        goto('/prodcut/10')
+    }
+</script>
+
+<button
+on:click={handleClick}
+>주문하기</button>
