ac12f8e2c3678e6fac2f7ff1bad1ca005f7144e1
tokkaiiii <luv_3_u@naver.com>
2024-11-19 13:38:43 +0900

    :sparkles: feat(day1-3): dom 예제
    - dom 예제
      - if

---
+++ src/components/example02/btnComp.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let handleClick
+    export let count
+    import LabelComp from "./labelComp.svelte";
+</script>
+
+<button
+        on:click={handleClick}
+>
+    클릭수 { count } <LabelComp {count} />
+</button>
---
+++ src/routes/example03/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    let auth = {
+        loggedIn: false
+    }
+    const handleLogin = () => auth.loggedIn = true
+    const handleLogout = () => auth.loggedIn = false
+</script>
+<button on:click={handleLogin}>Log In</button>
+<button on:click={handleLogout}>Log Out</button>
+
+<!--<p>{auth.loggedIn}</p>-->
+
+{#if auth.loggedIn === true }
+    <p>로그인 상태입니다.</p>
+{:else }
+    <p>로그아웃 상태입니다.</p>
+{/if}
