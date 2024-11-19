7113d7660159c2b1f9327b1e1518377d9c666ac9
tokkaiiii <luv_3_u@naver.com>
2024-11-19 11:46:17 +0900

    :sparkles: feat(day1-2): component 생성
    - component 생성
      - header
      - content
      - footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
     let count = 0;
+    $: doubled = count * 2
+    $: if (count >= 10) {
+        alert('카운트가 10을 넘었습니다.')
+        count = 9
+    }
+    $: {
+        console.log( count )
+        console.log( doubled )
+    }
     function handleClick(){
         count +=1;
     }
@@ -7,5 +16,6 @@
 <button
 on:click={handleClick}
 >
-    클릭수 { count }
+    클릭수 { count } {count ===1 ? 'time' : 'times'}
 </button>
+<p>{count} 두배는 {doubled} </p>
